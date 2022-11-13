3b331e8ca00c855d2574c9ac50c32f0e95fb6394
TzenCalvin <calvintzenftw@gmail.com>
2022-11-12 17:37:14 -0800

    Create aggregate table of data

---
+++ source/summary_table.R
@@ -0,0 +1,33 @@
+# summary_table.R 
+# Imports csv files into RStudio
+library(tidyverse)
+
+police_killings <- read.csv("~/info201/project/info201-project/data/PoliceKillingsUS.csv", stringsAsFactors = FALSE)
+median_household_income_2015 <- read.csv("~/info201/project/info201-project/data/MedianHouseholdIncome2015.csv", stringsAsFactors = FALSE)
+poverty_levels <- read.csv("~/info201/project/info201-project/data/PercentagePeopleBelowPovertyLevel.csv", stringsAsFactors = FALSE)
+complete_high_school <- read.csv("~/info201/project/info201-project/data/PercentOver25CompletedHighSchool.csv", stringsAsFactors = FALSE)
+demographic <- read.csv("~/info201/project/info201-project/data/ShareRaceByCity.csv", stringsAsFactors = FALSE)
+
+poverty_levels <- poverty_levels %>%
+  rename(state = Geographic.Area) %>%
+  mutate(city = str_remove_all(City, " city| town| village| CDP| borough")) %>%
+  select(-(City))
+
+median_household_income_2015 <- median_household_income_2015 %>%
+  rename(state = Geographic.Area) %>%
+  mutate(city = str_remove_all(City, " city| town| village| CDP| borough")) %>%
+  select(-(City))
+
+complete_high_school <- complete_high_school %>%
+  rename(state = Geographic.Area) %>%
+  mutate(city = str_remove_all(City, " city| town| village| CDP| borough")) %>%
+  select(-(City))
+
+demographic <- demographic %>%
+  rename(state = Geographic.area) %>%
+  mutate(city = str_remove_all(City, " city| town| village| CDP| borough")) %>%
+  select(-(City))
+
+aggregate <- list(police_killings, median_household_income_2015, complete_high_school, poverty_levels, demographic) %>%
+  reduce(left_join, by = c("city", "state")) %>%
+  group_by(date)
