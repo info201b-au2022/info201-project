cbf97d69e3e5d54d12532714a6dfb06d9fd2f594
TzenCalvin <calvintzenftw@gmail.com>
2022-11-13 18:29:00 -0800

    summary table updated

--- source/summary_table.R
+++ source/summary_table.R
@@ -8,6 +8,14 @@ poverty_levels <- read.csv("~/info201/project/info201-project/data/PercentagePeo
 complete_high_school <- read.csv("~/info201/project/info201-project/data/PercentOver25CompletedHighSchool.csv", stringsAsFactors = FALSE)
 demographic <- read.csv("~/info201/project/info201-project/data/ShareRaceByCity.csv", stringsAsFactors = FALSE)
 
+police_killings$race[police_killings$race == "A"] <- "Asian"
+police_killings$race[police_killings$race == "B"] <- "Black"
+police_killings$race[police_killings$race == "H"] <- "Hispanic"
+police_killings$race[police_killings$race == "W"] <- "White"
+police_killings$race[police_killings$race == "N"] <- "Native"
+police_killings$race[police_killings$race == "O"] <- "Other"
+police_killings$race[police_killings$race == ""] <- "Unknown"
+
 poverty_levels <- poverty_levels %>%
   rename(state = Geographic.Area) %>%
   mutate(city = str_remove_all(City, " city| town| village| CDP| borough")) %>%
