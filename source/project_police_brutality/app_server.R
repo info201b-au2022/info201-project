1b285ff9dcecfebb9e617468c2080560144a74a9
TzenCalvin <calvintzenftw@gmail.com>
2022-12-07 00:48:14 -0800

    Implement tab for chart 2

--- source/project_police_brutality/app_server.R
+++ source/project_police_brutality/app_server.R
@@ -3,6 +3,41 @@ library(shiny)
 library(ggplot2)
 
 server <- function(input, output) {
+  output$chart2 <- renderPlotly({
+    data <- police_killings %>%
+      filter(state == input$selectstate) %>%
+      group_by(race) %>%
+      summarize(
+        number_of_deaths = n()
+      )
+    
+    plot <- ggplot(data) +
+      geom_bar(
+        mapping = aes(y = number_of_deaths, x= race),
+        stat = "identity"
+      ) + 
+      labs(
+        x = "Race",
+        y = "Number of deaths",
+        title = paste("Deaths based on race in", input$selectstate, "from 2015 to 2017")
+      ) +
+      coord_flip()
+    
+    return(plot)
+  })
   
+  output$caption2 <- renderText({
+    caption <- "This graph displays the number of deaths that occur per race in 
+    a given state. When looking at different states. those with more diverse 
+    demographics such as California will see a wide variety in the race of those 
+    killed in violent police shootings, but those who are more majority-white 
+    such as Kentucky will see a much bigger ratio of White people killed. Native 
+    Americans, being a much smaller proportion of the population of most states, 
+    typically do not see very many deaths in any states, while White people, 
+    being the majority race in America, see a pretty high proportion of deaths 
+    cause by police officers in any state."
+    
+    return(caption)
+  })
 }
 
--- source/project_police_brutality/app_ui.R
+++ source/project_police_brutality/app_ui.R
@@ -5,6 +5,55 @@ library(tidyverse)
 library(plotly)
 library(shinythemes)
 
-ui <- navbarPage(
+police_killings <- read.csv("~/info201/project/info201-project/data/PoliceKillingsUS.csv", stringsAsFactors = FALSE)
+police_killings$race[police_killings$race == "A"] <- "Asian"
+police_killings$race[police_killings$race == "B"] <- "Black"
+police_killings$race[police_killings$race == "H"] <- "Hispanic"
+police_killings$race[police_killings$race == "W"] <- "White"
+police_killings$race[police_killings$race == "N"] <- "Native"
+police_killings$race[police_killings$race == "O"] <- "Other"
+police_killings$race[police_killings$race == ""] <- "Unknown"
 
+states <- unique(police_killings$state)
+
+interactive_2_sidebar <- sidebarPanel(
+  selectInput(
+    inputId = "selectstate",
+    label = "Select a state",
+    choices = states
+  )
+  #, selectInput(
+  #   inputId = "selectvar",
+  #   label = "Desired Emission Type",
+  #   choices = c(
+  #     "Overall including land-use change (CO2)" = "co2_including_luc", 
+  #     "Overall excluding land-use change (CO2)" = "co2", 
+  #     "Coal (CO2)" = "coal_co2",
+  #     "Flaring (CO2)" = "flaring_co2",
+  #     "Cement (CO2)" = "cement_co2",
+  #     "Gas (CO2)" = "gas_co2",
+  #     "Land-use change (CO2)" = "land_use_change_co2",
+  #     "Oil (CO2)" = "oil_co2"
+  #   )
+  # )
+)
+
+interactive_2_chart <- mainPanel(
+  plotlyOutput("chart2"),
+  textOutput("caption2")
+)
+
+interactive_2_panel <- tabPanel(
+  title = "Chart 2",
+  titlePanel("Deaths Based on Race in Different States Interactive Graph"),
+  sidebarLayout(
+    interactive_2_sidebar,
+    interactive_2_chart
+  )
+)
+
+ui <- navbarPage(
+  theme = shinytheme("cerulean"),
+  "Gun Violence",
+  interactive_2_panel
 )
