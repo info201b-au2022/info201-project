cf6d712b5f5b3afb4dc51a171988319c03e352df
Lesley <xuchenran0502@126.com>
2022-12-07 22:30:03 -0800

    intro page

--- source/project_police_brutality/app_server.R
+++ source/project_police_brutality/app_server.R
@@ -3,6 +3,7 @@ library(shiny)
 library(ggplot2)
 
 server <- function(input, output) {
+
   output$chart1 <- renderPlot({ 
     df <- police_killings %>%
       filter(state == input$stateselected) %>%
